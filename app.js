cb24e08e7d6b484fe468eaf5384277bc357e51ee
WenleMeka <wenlemeca123@gmail.com>
2022-01-18 20:19:02 +0100

    init project

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log(Hola)
+
+const formulario = document.getElementById('formulario');;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+const listaTareas = document.getElementById('Lista-tareas')
+const template = document.getElementById('template')
+const fragment = document.createDocumentFragment()
+let tareas = {}
+
+formulario.addEventListener('submit', e => {
+    console.log('procesando formulario');
+})
