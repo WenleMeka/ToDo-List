ea015205b6b2937cc6920ed84dd76eb2199f97ce
WenleMeka <wenlemeca123@gmail.com>
2022-01-25 20:43:48 +0100

    añadir console log(por finalizar)

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-console.log(Hola)
+console.log('Hola')
 
-const formulario = document.getElementById('formulario');;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-const listaTareas = document.getElementById('Lista-tareas')
-const template = document.getElementById('template')
+const formulario = document.getElementById('formulario')
+const listaTareas = document.getElementById('lista-tareas')
+const template = document.getElementById('template').content
 const fragment = document.createDocumentFragment()
 let tareas = {}
 
+
+
 formulario.addEventListener('submit', e => {
-    console.log('procesando formulario');
+    e.preventDefault()
+    // console.log(e.target[0].value)
+    // console.log(e.target.querySelector('input').value)
+    setTarea(e)
 })
+
+const setTarea = e => {
+    const texto = e.target.querySelector('input').value
+    
+    if (texto.trim() === '') {
+        console.log('está vacio')
+        return
+    }
+    const tarea = {
+        id: Date.now(),
+        texto: texto,
+        estado: false
+    }
+
+    tareas[tarea.id] = tarea
+    pintarTareas()
+
+    formulario.reset()
+    e.target.querySelector('input').focus()
+}
